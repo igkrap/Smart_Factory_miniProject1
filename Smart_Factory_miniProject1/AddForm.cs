304a3efbd20fdedcd95c52d266cc3a496c4ff837
igkrap <parkgi45@gmail.com>
2021-05-27 09:30:35 +0900

    초기 커밋.

---
+++ Smart_Factory_miniProject1/AddForm.cs
@@ -0,0 +1,21 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_Factory_miniProject1
+{
+    public partial class AddForm : MetroForm
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Smart_Factory_miniProject1/MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Smart_Factory_miniProject1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.SuspendLayout();
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(291, 158);
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(188, 192);
+            this.metroProgressSpinner1.TabIndex = 2;
+            this.metroProgressSpinner1.Text = "metroProgressSpinner1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroProgressSpinner1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+    }
+}
+
